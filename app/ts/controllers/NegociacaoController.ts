46860a03ccdf2a81ec876a055f1df5e6fbca5b66
eddyeboy27 <lipe0503@gmail.com>
2021-06-07 03:59:00 -0300

    Finalizado capitulo de API Externa e Interface

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,82 +1,77 @@
 import { MensagemView, NegociacoesView } from '../views/index';
-import { Negociacoes, Negociacao } from '../models/index';
-import { domInject } from '../helpers/decorators/index';
+import { Negociacoes, Negociacao, NegociacaoParcial } from '../models/index';
+import { domInject, throttle } from '../helpers/decorators/index';
+import { NegociacaoService, ResponseHandler } from '../services/index';
+
+let timer: number = 0;
 
 export class NegociacaoController {
+	@domInject('#data')
+	private _inputData: JQuery;
 
-    @domInject('#data')
-    private _inputData: JQuery;
+	@domInject('#quantidade')
+	private _inputQuantidade: JQuery;
 
-    @domInject('#quantidade')
-    private _inputQuantidade: JQuery;
+	@domInject('#valor')
+	private _inputValor: JQuery;
+	private _negociacoes = new Negociacoes();
+	private _negociacoesView = new NegociacoesView('#negociacoesView');
+	private _mensagemView = new MensagemView('#mensagemView');
+  private _service = new NegociacaoService();
 
-    @domInject('#valor')
-    private _inputValor: JQuery;
-    private _negociacoes = new Negociacoes();
-    private _negociacoesView = new NegociacoesView('#negociacoesView');
-    private _mensagemView = new MensagemView('#mensagemView');
+	constructor() {
+		this._negociacoesView.update(this._negociacoes);
+	}
 
-    constructor() {
+	@throttle()
+	importarDados() {
+		const isOk: ResponseHandler = (res: Response) => {
+			if (res.ok) {
+				return res;
+			} else {
+				throw new Error(res.statusText);
+			}
+		}
+    this._service
+      .obterNegociacoes(isOk)
+      .then(negociacoes => {
+        negociacoes.forEach(negociacao => this._negociacoes.adiciona(negociacao))
+      });
         this._negociacoesView.update(this._negociacoes);
-    }
+  }
 
-    importarDados() {
+  @throttle()
+	adiciona(event: Event) {
+		const t1 = performance.now();
+		event.preventDefault();
 
-        function isOk(res: Response) {
-            if(res.ok) {
-                return res;
-            } else {
-                throw new Error(res.statusText);
-            }
-        }
+		let data = new Date(this._inputData.val().replace(/-/g, ','));
 
-        fetch('http://localhost:9090/dados')
-            .then(res => isOk(res))
-            .then(res => res.json())
-            .then((dados: any[]) => {
-                dados
-                    .map(dado => new Negociacao(new Date(), dado.vezes, dado.montante))
-                    .forEach(negociacao => this._negociacoes.adiciona(negociacao));
-                this._negociacoesView.update(this._negociacoes);
-            })
-            .catch(err => console.log(err.message));
-    }
+		if (!this._ehDiaUtil(data)) {
+			this._mensagemView.update('Somente negociações em dias úteis, por favor!');
+			return;
+		}
 
-    adiciona(event: Event) {
-        const t1 = performance.now();
-        event.preventDefault();
+		const negociacao = new Negociacao(data, parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
 
-        let data = new Date(this._inputData.val().replace(/-/g, ','));
+		this._negociacoes.adiciona(negociacao);
+		this._negociacoesView.update(this._negociacoes);
+		this._mensagemView.update('Negociação adicionada com sucesso!');
+		const t2 = performance.now();
+		console.log(`Tempo de execução do método adiciona(): ${(t2 - t1) / 1000} segundos`);
+	}
 
-        if (!this._ehDiaUtil(data)) {
-            this._mensagemView.update('Somente negociações em dias úteis, por favor!');
-            return
-        }
-
-        const negociacao = new Negociacao(
-            data,
-            parseInt(this._inputQuantidade.val()),
-            parseFloat(this._inputValor.val())
-        );
-
-        this._negociacoes.adiciona(negociacao);
-        this._negociacoesView.update(this._negociacoes);
-        this._mensagemView.update('Negociação adicionada com sucesso!');
-        const t2 = performance.now();
-        console.log(`Tempo de execução do método adiciona(): ${(t2 - t1) / 1000 } segundos`)
-    }
-
-    private _ehDiaUtil(data: Date) {
-        return data.getDay() !== DiaDaSemana.Sabado && data.getDay() !== DiaDaSemana.Domingo
-    }
+	private _ehDiaUtil(data: Date) {
+		return data.getDay() !== DiaDaSemana.Sabado && data.getDay() !== DiaDaSemana.Domingo;
+	}
 }
 
 enum DiaDaSemana {
-  Domingo,
-  Segunda,
-  Terca,
-  Quarta,
-  Quinta,
-  Sexta,
-  Sabado,
+	Domingo,
+	Segunda,
+	Terca,
+	Quarta,
+	Quinta,
+	Sexta,
+	Sabado,
 }
